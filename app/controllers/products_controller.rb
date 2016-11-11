d83b8bad53d364a07c6f0309233999ac6c8ff746
Yvonne Ike <yviedev@gmail.com>
2016-11-10 21:31:22 -0500

    Update app

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,26 @@ class ProductsController < ApplicationController
     @products = Product.all
     render 'all_products.html'
   end
+
+  def new_product_form
+    @title = "Product form"
+    render 'new_form.html.erb'
+  end
+
+  def make_product
+    @title = "Added product"
+    @name = params["product_name"]
+    @price = params["product_price"]
+    @image = params["product_image"]
+    @description = params["product_description"]
+    
+    @product = Product.new(
+      name: params["product_name"],
+      price: params["product_price"],
+      image: params["product_image"],
+      description: params["product_description"]
+      )
+    @product.save
+    render 'make_product.html.erb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get 'all' => 'products#all_products'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # place to show the user form
+  get '/product_form' => 'products#new_product_form'
+  # place to show the user that they created a new product
+  post '/created_products' => 'products#make_product'
 end
